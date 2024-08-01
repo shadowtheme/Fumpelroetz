e987f753bca7652eb4e64526b466c653aedb345c
shadowtheme <121387628+shadowtheme@users.noreply.github.com>
2024-08-01 14:57:25 +0200

    Create Scene.cs

---
+++ Scene.cs
@@ -0,0 +1,8 @@
+public class Scene
+{
+    public string Name { get; set; }
+    public int PanelX { get; set; }
+    public int PanelY { get; set; }
+    public int PanelWidth { get; set; }
+    public int PanelHeight { get; set; }
+}
