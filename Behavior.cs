e52a2b7855bd68b0d5ab671b1f5fb03effdca853
shadowtheme <121387628+shadowtheme@users.noreply.github.com>
2024-08-02 00:01:21 +0200

    Update and rename Behavior to Behavior.cs

---
+++ Behavior.cs
@@ -0,0 +1,35 @@
+namespace EscapeRoomControlPanel
+{
+    public abstract class Behavior
+    {
+        public abstract string Name { get; }
+        public abstract string Description { get; }
+    }
+
+    public class PinStatusBehavior : Behavior
+    {
+        public override string Name => "PinStatus";
+        public override string Description => "Beschreibung für PinStatus abfragen";
+    }
+
+    public class SzeneGestartetBehavior : Behavior
+    {
+        public override string Name => "Szene gestarted";
+        public override string Description => "Beschreibung abfragen";
+    }
+
+    public class AudioStartetBehavior : Behavior
+    {
+        public override string Name => "Audio startet";
+        public override string Description => "Beschreibung abfragen";
+    }
+
+    public class AudioStopptBehavior : Behavior
+    {
+        public override string Name => "Audio stoppt";
+        public override string Description => "Beschreibung abfragen";
+    }
+
+}
+
+
