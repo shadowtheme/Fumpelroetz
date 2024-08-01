bc836f4f55c04ef9b4a5e1d2de29d1f900b252bd
shadowtheme <121387628+shadowtheme@users.noreply.github.com>
2024-08-01 22:24:34 +0200

    Create BehaviorManager.cs

---
+++ BehaviorManager.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+
+namespace EscapeRoomControlPanel
+{
+    public class BehaviorManager
+    {
+        private List<Behavior> behaviors;
+
+        public BehaviorManager()
+        {
+            behaviors = new List<Behavior>();
+        }
+
+        public void AddBehavior(Behavior behavior)
+        {
+            behaviors.Add(behavior);
+        }
+
+        public List<Behavior> GetBehaviors()
+        {
+            return behaviors;
+        }
+    }
+}
+
