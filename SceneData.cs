8b6f4e2ee362fc0daabfd7e7a91eab3ad7f82dbd
shadowtheme <121387628+shadowtheme@users.noreply.github.com>
2024-08-01 14:58:27 +0200

    Create SceneData.cs

---
+++ SceneData.cs
@@ -0,0 +1,9 @@
+namespace EscapeRoomControlPanel
+{
+    public class SceneData
+    {
+        public string Name { get; set; }
+        public int LocationX { get; set; }
+        public int LocationY { get; set; }
+    }
+}
